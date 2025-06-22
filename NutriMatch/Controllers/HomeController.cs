b57b113ea6db53b5177f9be54b03eef6d2223865
Milan Stojanoski <milan.stojanoski04@gmail.com>
2025-06-22 22:10:06 +0200

    Landing page design

--- NutriMatch/Controllers/HomeController.cs
+++ NutriMatch/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NutriMatch.Data;
+using NutriMatch.Models;
 
 namespace MyApp.Namespace
 {
     public class Home : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        private readonly AppDbContext _context;
+        public Home(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Recipe> recipes = await _context.Recipes.Take(6).ToListAsync();
+            return View(recipes);
         }
 
        
